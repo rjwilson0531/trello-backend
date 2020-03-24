c05864414808b2823db135334927a52bbe726319
rjwilson0531 <rjwilson0531@gmail.com>
2020-03-24 10:50:42 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding data"
+
+Card.delete_all
+Item.delete_all
+
+card1 = Card.create(title:"To do")
+card2 = Card.create(title:"In progress")
+card3 = Card.create(title:"Complete")
+
+item1 = Item.create(title:"Do a 360 Dab", content:"Dab Content", card_id: card1.id)
+item2 = Item.create(title:"Finish MVP", content:"A lot of stuff to do", card_id: card1.id)
+item3 = Item.create(title:"Get hired at Google", content:"Easy.", card_id: card2.id)
