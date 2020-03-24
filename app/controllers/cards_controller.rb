5bc59c48c0b6084868ee109e378c51676d8e2b41
rjwilson0531 <rjwilson0531@gmail.com>
2020-03-24 11:05:07 -0400

    added index/create to items and cards controllers

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,16 @@
 class CardsController < ApplicationController
+    def index
+        cards = Card.all
+        render json: cards.to_json(
+            :except => [:created_at, :updated_at],
+            :include => {:items =>
+                {:except => [:created_at, :updated_at]}
+            }
+        )
+    end
+
+    def create
+        puts "Hit create method in card_controller. Look at the params hash"
+        puts params unless !params
+    end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items.to_json(
+            :except => [:created_at, :updated_at]
+        )
+    end
+
+    def create
+        puts "Hit create method in item_controller. Look at the params hash"
+        puts params unless !params
+    end
 end
