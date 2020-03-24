4809a289b8cc2dc905bf7f9dd7429b5b2f820c4e
rjwilson0531 <rjwilson0531@gmail.com>
2020-03-24 10:36:28 -0400

    added card_id to items

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_142926) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.integer "card_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
