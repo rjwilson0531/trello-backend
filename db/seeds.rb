9790efa1c75ec6625abf98d940bfe18105b9027d
rjwilson0531 <rjwilson0531@gmail.com>
2020-03-25 14:08:59 -0400

    better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 puts "Seeding data"
-
-Card.delete_all
-Item.delete_all
+DatabaseCleaner.clean_with(:truncation)
 
 card1 = Card.create(title:"To do")
 card2 = Card.create(title:"In progress")
 card3 = Card.create(title:"Complete")
+card4 = Card.create(title:"Stretch features")
 
-item1 = Item.create(title:"Do a 360 Dab", content:"Dab Content", card_id: card1.id)
-item2 = Item.create(title:"Finish MVP", content:"A lot of stuff to do", card_id: card1.id)
-item3 = Item.create(title:"Get hired at Google", content:"Easy.", card_id: card2.id)
+item1 = Item.create(title:"Edit Card Title onclick", content:"Content", card_id: card1.id)
+item2 = Item.create(title:"Make close button (delete card)", content:"Content", card_id: card1.id)
+item3 = Item.create(title:"Delete card confirmation + items", content:"Content", card_id: card1.id)
+item4 = Item.create(title:"Items change card_id when swapping cards", content:"Content", card_id: card1.id)
+item5 = Item.create(title:"View content on div.title click", content:"Content", card_id: card1.id)
+item6 = Item.create(title:"Make edit button ", content:"Content", card_id: card1.id)
+item7 = Item.create(title:"Edit button changed title", content:"Content", card_id: card1.id)
+item8 = Item.create(title:"Comments", content:"Content", card_id: card4.id)
+item9 = Item.create(title:"Cards can be sorted", content:"Content", card_id: card4.id)
+item10 = Item.create(title:"Items can be sorted", content:"Content", card_id: card4.id)
+item11 = Item.create(title:"Refactor", content:"Content", card_id: card4.id)
+item12 = Item.create(title:"Create Card", content:"Content", card_id: card3.id)
+item13 = Item.create(title:"Create Item", content:"Content", card_id: card3.id)
+item14 = Item.create(title:"Setup Rails api", content:"Content", card_id: card3.id)
